4e881db14c1b1b940bbdf9d9f9774d3d197994f1
danilka108 <danilka108@yandex.ru>
2024-05-24 02:55:02 +0700

    Updates

---
+++ static/js/312.66bea963.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmoex_widget=self.webpackChunkmoex_widget||[]).push([[312],{6931:(e,n,s)=>{s.r(n),s.d(n,{default:()=>x});var r=s(5117),c=s(5043),d=s(579);const t=()=>(0,d.jsx)(r.es,{children:(0,d.jsx)(r.b7,{name:"App",children:(0,d.jsx)(r.uA,{name:"Loader",children:(0,d.jsx)(r.KT,{children:(0,d.jsx)(a,{})})})})});function a(){return(0,d.jsx)(c.Fragment,{children:"Loading..."})}var i=s(5943);const x=()=>(0,d.jsx)(r.m8,{palette:(0,d.jsx)(t,{}),children:(0,d.jsx)(r.eu,{path:"/App",children:(0,d.jsx)(i.A,{})})})}}]);
+//# sourceMappingURL=312.66bea963.chunk.js.map
